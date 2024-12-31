c32fa70bc1754c8d8153cc62009bca65eb03acc2
Talimul212 <islamtalimul0@gmail.com>
2024-12-31 19:55:39 +0600

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+} from "./Components/redux/features/counter/counterSlice";
+
 function App() {
+  const dispatch = useDispatch();
+  const { count } = useSelector((state) => state?.counter);
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
   return (
     <>
       <div></div>
 
       <div>
-        <button style={{ marginRight: "20px" }}>Increment</button>0
-        <button style={{ marginLeft: "20px" }}>Decrement</button>
+        <button onClick={handleIncrement} style={{ marginRight: "20px" }}>
+          Increment
+        </button>
+        {count}
+        <button onClick={handleDecrement} style={{ marginLeft: "20px" }}>
+          Decrement
+        </button>
       </div>
     </>
   );
